c0dd8f81fc4fbff4d9d1f7f5bf8e433a1914c603
rodionmatytsin <79132260269@rambler.ru>
2024-11-05 16:14:45 +0700

    fix

--- main/views/views.py
+++ main/views/views.py
@@ -6,3 +6,18 @@ from fastapi.responses import HTMLResponse
 @main.get("/", status_code=200, tags=["Views"], response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
+
+
+@main.get("/login", status_code=200, tags=["Views"], response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", context={"request": request})
+
+
+@main.get("/gifts", status_code=200, tags=["Views"], response_class=HTMLResponse)
+async def gifts(request: Request):
+    return templates.TemplateResponse("prizesPage.html", context={"request": request})
+
+
+@main.get("/stage_two", status_code=200, tags=["Views"], response_class=HTMLResponse)
+async def stage_two(request: Request):
+    return templates.TemplateResponse("gameStageTwo.html", context={"request": request})
